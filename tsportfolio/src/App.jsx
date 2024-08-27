b781aee66b3b6c95c471b63b05ce0d145a469e3d
Tianna <140460149+tianna-spears@users.noreply.github.com>
2024-08-27 21:12:27 +0100

    project setup

---
+++ tsportfolio/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+      <h1> Tianna Spears, Front-End Developer </h1>
+      </div>
+    </>
+
+  )
+}
+
+export default App
